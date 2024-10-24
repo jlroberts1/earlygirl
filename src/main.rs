5671b38aee6f892d1788f52ad073df4b4578fc14
James Roberts <james.lee.roberts01@gmail.com>
2024-10-23 23:33:55 -0400

    Move timer settings into modal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use iced::keyboard;
 use iced::time;
-use iced::widget::{button, center, column, progress_bar, row, text};
-use iced::{Center, Element, Subscription, Theme};
+use iced::widget::{button, column, progress_bar, row, text, Column, Container, Text};
+use iced::Length;
+use iced::{Center, Element};
+use iced::{Subscription, Theme};
 
 use std::time::{Duration, Instant};
 
@@ -16,10 +18,11 @@ struct Timer {
     theme: Theme,
     current_timer_duration: f64,
     interval: f64,
-    work_interval: f64,
-    break_interval: f64,
     timer_type: TimerType,
     state: State,
+    work_interval: f64,
+    break_interval: f64,
+    show_modal: bool,
 }
 
 impl Default for Timer {
@@ -28,10 +31,11 @@ impl Default for Timer {
             theme: Theme::CatppuccinMocha,
             current_timer_duration: 0.0,
             interval: 10.0 * 60.0,
-            work_interval: 10.0 * 60.0,
-            break_interval: 5.0 * 60.0,
             timer_type: TimerType::WorkTime,
             state: State::Idle,
+            work_interval: 40.0 * 60.0,
+            break_interval: 5.0 * 60.0,
+            show_modal: false,
         }
     }
 }
@@ -55,6 +59,7 @@ enum State {
 enum Message {
     Toggle,
     Tick(Instant),
+    ToggleSettings,
     WorkIntervalChanged(f64),
     BreakIntervalChanged(f64),
     Reset,
@@ -135,6 +140,9 @@ impl Timer {
                     TimerType::BreakTime => self.break_interval,
                 };
             }
+            Message::ToggleSettings => {
+                self.show_modal = !self.show_modal;
+            }
         }
     }
 
@@ -149,7 +157,7 @@ impl Timer {
 
             match key.as_ref() {
                 keyboard::Key::Named(key::Named::Space) => Some(Message::Toggle),
-                // keyboard::Key::Character("r") => Some(Message::Reset),
+                keyboard::Key::Character("r") => Some(Message::Reset),
                 _ => None,
             }
         }
@@ -157,49 +165,8 @@ impl Timer {
         Subscription::batch(vec![tick, keyboard::on_key_press(handle_hotkey)])
     }
 
-    fn view(&self) -> Element<Message> {
+    fn settings_modal(&self) -> Element<Message> {
         const MINUTE: f64 = 60.0;
-        const HOUR: f64 = 60.0 * MINUTE;
-        let time_remaining = if self.interval <= self.current_timer_duration {
-            0.0
-        } else {
-            self.interval - self.current_timer_duration
-        };
-
-        let duration = text!(
-            "{:0>2}:{:0>2}:{:0>2}",
-            (time_remaining / HOUR).floor(),
-            ((time_remaining % HOUR) / MINUTE).floor(),
-            (time_remaining % MINUTE).floor()
-        )
-        .size(80);
-
-        let button = |label| button(text(label).align_x(Center)).padding(10).width(80);
-
-        let toggle_button = {
-            let label = match self.state {
-                State::Idle => "Start",
-                State::Ticking { .. } => "Pause",
-            };
-            button(label).on_press(Message::Toggle)
-        };
-
-        let reset_button = button("Reset").on_press(Message::Reset);
-        let switch_timer_type_button = button("Switch").on_press(Message::SwitchWorkType);
-
-        let working_label = match self.state {
-            State::Ticking { .. } => "Working!",
-            State::Idle => "Start Working!",
-        };
-
-        let timer_label = {
-            let label = match self.timer_type {
-                TimerType::WorkTime => working_label,
-                TimerType::BreakTime => "Break Time!",
-            };
-            text(label).size(30)
-        };
-
         let work_slider = iced::widget::slider(
             5.0..=60.0,
             self.work_interval / MINUTE,
@@ -216,22 +183,105 @@ impl Timer {
         .step(5)
         .width(200);
 
+        Column::new()
+            .spacing(20)
+            .padding(20)
+            .push(Text::new("Set Work Time (minutes)"))
+            .push(work_slider)
+            .push(Text::new("Set Break Time(minutes)"))
+            .push(break_slider)
+            .into()
+    }
+
+    fn view(&self) -> Element<Message> {
+        const MINUTE: f64 = 60.0;
+        const HOUR: f64 = 60.0 * MINUTE;
+
+        let time_remaining = if self.interval <= self.current_timer_duration {
+            0.0
+        } else {
+            self.interval - self.current_timer_duration
+        };
+        let duration = text!(
+            "{:0>2}:{:0>2}:{:0>2}",
+            (time_remaining / HOUR).floor(),
+            ((time_remaining % HOUR) / MINUTE).floor(),
+            (time_remaining % MINUTE).floor()
+        )
+        .size(80);
+
+        let settings_button = {
+            let label = match self.show_modal {
+                true => "Hide Settings",
+                false => "Show Settings",
+            };
+            button(text(label)).on_press(Message::ToggleSettings)
+        };
+
+        let start_pause_button = {
+            let label = match self.state {
+                State::Idle => "Start",
+                State::Ticking { .. } => "Pause",
+            };
+            button(label)
+                .style(|theme: &Theme, status| {
+                    let palette = theme.extended_palette();
+                    match status {
+                        button::Status::Active => {
+                            button::Style::default().with_background(palette.success.strong.color)
+                        }
+                        _ => button::primary(theme, status),
+                    }
+                })
+                .on_press(Message::Toggle)
+        };
+
+        let reset_button = button("Reset")
+            .style(|theme: &Theme, status| {
+                let palette = theme.extended_palette();
+                match status {
+                    button::Status::Active => {
+                        button::Style::default().with_background(palette.secondary.strong.color)
+                    }
+                    _ => button::primary(theme, status),
+                }
+            })
+            .on_press(Message::Reset);
+
+        let switch_timer_type_button = button("Switch").on_press(Message::SwitchWorkType);
+
+        let working_label = match self.state {
+            State::Ticking { .. } => "Working!",
+            State::Idle => "Start Working!",
+        };
+
+        let timer_label = {
+            let label = match self.timer_type {
+                TimerType::WorkTime => working_label,
+                TimerType::BreakTime => "Break Time!",
+            };
+            text(label).size(30)
+        };
+
         let timer_progress = (self.current_timer_duration / self.interval) * 100.0;
         let progress_bar = progress_bar(0.0..=100.0, timer_progress as f32);
-        let row = row![toggle_button, switch_timer_type_button, reset_button,].spacing(20);
-        let content = column![
-            timer_label,
-            duration,
-            progress_bar,
-            row,
-            text("Set Work Time (minutes):"),
-            work_slider,
-            text("Set Break Time (minutes):"),
-            break_slider,
-        ]
-        .align_x(Center)
-        .padding(20)
-        .spacing(20);
-        center(content).into()
+
+        let row = row![start_pause_button, switch_timer_type_button, reset_button,].spacing(20);
+        let mut content = column![timer_label, duration, progress_bar, row,]
+            .align_x(Center)
+            .padding(20)
+            .spacing(20);
+
+        content = content.push(settings_button);
+
+        if self.show_modal {
+            let modal = self.settings_modal();
+            content = content.push(modal);
+        }
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
     }
 }
